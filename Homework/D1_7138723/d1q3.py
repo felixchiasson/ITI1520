ed6201332607986b54209ba728a46a466dc3cfe3
Félix Chiasson <github@felixchiasson.ca>
2015-10-03 10:28:18 -0400

    Again, else: is not wanted here. Not sure why I thought it was a good idea to copy and paste it everywhere.

--- Homework/D1_7138723/d1q3.py
+++ Homework/D1_7138723/d1q3.py
@@ -3,7 +3,7 @@
 #     File Name           :     d1q3.py
 #     Created By          :     Félix Chiasson (7138723)
 #     Creation Date       :     [2015-09-19 15:21]
-#     Last Modified       :     [2015-09-19 23:00]
+#     Last Modified       :     [2015-10-03 10:27]
 #     Description         :     Converts pounds and ounces to kilograms
 ################################################################################
 import sys
@@ -16,13 +16,7 @@ while not positif:
     try:
         pounds = float(input("Veuillez entrer le nombre de livres: "))
         ounces = float(input("Veuillez entrer le nombre d'onces: "))
-    except ValueError:
-        print("Vous n'avez pas entré une valeur numérique pour l'une des deux"\
-                " options")
-    except KeyboardInterrupt:
-        print("\nAurevoir...")
-        sys.exit()
-    else:
+
         if pounds < 0 or ounces < 0:
             print("Votre valeur est négative. Veuillez entrer une valeur"\
                    " positive.")
@@ -31,3 +25,11 @@ while not positif:
             print("{0:.1f} livres et {1:.1f} onces équivalent à {2:.4f} "\
                     "kilogrammes.".format(pounds, ounces, kilograms))
             positif = True
+
+    except ValueError:
+        print("Vous n'avez pas entré une valeur numérique pour l'une des deux"\
+                " options")
+    except KeyboardInterrupt:
+        print("\nAurevoir...")
+        sys.exit()
+
