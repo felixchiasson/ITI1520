ffbff0066567e338ef4247d0ca3a3ec6c8415ca0
Félix Chiasson <github@felixchiasson.ca>
2015-10-03 10:23:52 -0400

    Cases like 1.43 gave a wrong output. Turns out the floor divison of 0.03 by 0.01 is 2.0. Not sure why. Got rid of else: as it wasn't necessary. Fixed more rounding issues that would lead to huge modulo floats instead of an actual dollar value.

--- Homework/D1_7138723/d1q2.py
+++ Homework/D1_7138723/d1q2.py
@@ -3,7 +3,7 @@
 #     File Name           :     d1q2.py
 #     Created By          :     Félix Chiasson (7138723)
 #     Creation Date       :     [2015-09-19 16:36]
-#     Last Modified       :     [2015-09-19 23:07]
+#     Last Modified       :     [2015-10-03 10:20]
 #     Description         :     Calcule le nombre minimal de pièce de monnaie 
 #                               nécessaire pour une valeur en $ donnée.
 ################################################################################
@@ -12,34 +12,40 @@ positif = False
 while not positif:
     try:
         montant = float(input('Entrez le montant en dollars: '))
-    except ValueError:
-        print("Veuillez entrer une valeur numérique.")
-    except KeyboardInterrupt:
-        print("\nAurevoir...")
-        sys.exit()
-    else:
 
         if montant < 0:
             print("Veuillez entrer une valeur numérique positive.")
         else:
             qMod = montant % 0.25
+            qMod = round(qMod, 2)
             nbQuarters = montant // 0.25
-
+            
             nbDimes = qMod // 0.10
-            dMod = round(qMod, 2) % 0.10    # round() est utilisé afin d'éviter
+            dMod = qMod % 0.10              # round() est utilisé afin d'éviter
                                             # les cas où le modulo est 0.199 
                                             # où l'on obtiendrais 1 pièce 
                                             # de moins que la vraie réponse
+            dMod = round(dMod, 2)
 
             nbNickels = dMod // 0.05
-            nMod = round(dMod, 2) % 0.05
-
-            nbPennies = nMod // 0.01
+            nMod = dMod % 0.05
+            nMod = round(nMod, 2)
+            
+            nbPennies = nMod / 0.01         # Floor division does not seem to
+                                            # work here. Using true division.
             pMod = round(nMod, 2) % 0.01
 
             nbMin = nbQuarters + nbNickels + nbDimes + nbPennies
             print("Le nombre minimum de pièces est: {0:.0f}".format(nbMin))
+
             positif = True
+
+
+    except ValueError:
+        print("Veuillez entrer une valeur numérique.")
+    except KeyboardInterrupt:
+        print("\nAurevoir...")
+        sys.exit()
 
 
 
