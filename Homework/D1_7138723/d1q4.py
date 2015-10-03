96d9d100cc0272e47f677eca300a24b86ab4723a
Félix Chiasson <github@felixchiasson.ca>
2015-10-03 10:33:32 -0400

    Added the fix from ae00a53afecf7209f4a27e86d1c309a612580950 and got rid of the unnecessary else:

--- Homework/D1_7138723/d1q4.py
+++ Homework/D1_7138723/d1q4.py
@@ -3,7 +3,7 @@
 #     File Name           :     d1q4.py
 #     Created By          :     Félix Chiasson (7138723)
 #     Creation Date       :     [2015-09-19 16:36]
-#     Last Modified       :     [2015-09-22 13:55]
+#     Last Modified       :     [2015-10-03 10:32]
 #     Description         :     Calcule le nombre minimal de pièce de monnaie 
 #                               nécessaire pour une valeur en $ donnée.
 ################################################################################
@@ -11,16 +11,19 @@ import sys
 
 def piecesMin(amount):                  # Fonction qui calcule le nombre
     qM = amount % 0.25                  # minimal de pièces nécessaires
+    qM = round(qM, 2)
     nbQ = amount // 0.25
 
     nbD = qM // 0.10
-    dM = round(qM, 2) % 0.10
+    dM = qM % 0.10
+    dM = round(dM, 2)
 
     nbN = dM // 0.05
-    nM = round(dM,2) % 0.05
+    nM = dM % 0.05
+    nM = round(nM, 2)
         
-    nbP = nM // 0.01
-    pM = round(nM,2) % 0.01
+    nbP = nM / 0.01                     # Floor division does not work properly
+                                        # in this case.
 
     return nbQ + nbD + nbN + nbP
 
@@ -28,12 +31,6 @@ positif = False
 while not positif:
     try:
         montant = float(input('Entrez le montant en dollars: '))
-    except ValueError:
-        print("Veuillez entrer une valeur numérique.")
-    except KeyboardInterrupt:
-        print("\nAurevoir...")
-        sys.exit()
-    else:
 
         if montant < 0:
             print("Veuillez entrer une valeur numérique positive.")
@@ -42,4 +39,11 @@ while not positif:
             print("Le caissier aura besoin d'un minimum de {0:.0f}"\
                     " pièces.".format(nbMin))
             positif = True
+
+    except ValueError:
+        print("Veuillez entrer une valeur numérique.")
+    except KeyboardInterrupt:
+        print("\nAurevoir...")
+        sys.exit()
+
 
