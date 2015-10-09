238d096a42a48137048bbd9d92dc775a1ae40cf8
Félix Chiasson <github@felixchiasson.ca>
2015-10-09 17:06:34 -0400

    Added question 2

---
+++ Homework/D2_7138723/d2q2.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+###############################################################################
+#     File Name           :     l4e2.py
+#     Created By          :     Félix Chiasson (7138723)
+#     Creation Date       :     [2015-10-06 11:35]
+#     Last Modified       :     [2015-10-09 17:04]
+#     Description         :     Print numbers from a to b  
+###############################################################################
+import sys
+
+def printNb(a, b):
+    "Prints numbers from a to b"
+    while a <= b:
+        print(a,end=" ")
+        a = a + 1
+
+s = False
+while not s:
+    try:
+        n = int(input("Veuillez entrer le premier numéro: "))
+        m = int(input("Veuillez entrer le deuxième numéro: "))
+        if n > m:
+            print("Votre premier chiffre est plus grand que le deuxième. Ce "
+                    "programme compte en ordre croissant!")
+
+        else:
+            printNb(n, m)
+            s = True
+
+    except ValueError:      # Si l'utilisateur entre une lettre ou un symbol
+        print("Veuillez entrer une valeur numérique")
+    except KeyboardInterrupt:
+        print("\n...Aurevoir!")
+        sys.exit()
