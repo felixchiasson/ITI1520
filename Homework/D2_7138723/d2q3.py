1366c4d00db6953635d5c4defc24cdd539baf5f7
Félix Chiasson <github@felixchiasson.ca>
2015-10-11 12:41:21 -0400

    Commented

--- Homework/D2_7138723/d2q3.py
+++ Homework/D2_7138723/d2q3.py
@@ -3,7 +3,7 @@
 #     File Name           :     d2q3.py
 #     Created By          :     Félix Chiasson (7138723)
 #     Creation Date       :     [2015-10-11 10:56]
-#     Last Modified       :     [2015-10-11 11:58]
+#     Last Modified       :     [2015-10-11 12:24]
 #     Description         :     Outil d'apprentissage de multiplications et
 #                               d'addition
 ###############################################################################
@@ -13,8 +13,11 @@ import operator
 from random import randint
 
 def jeuAM(c):
-    operators = {"+": operator.add,
-                 "*": operator.mul}
+
+############# Initialisation des variables locales ###############
+    
+    operators = {"+": operator.add,     # Dictionnary used with the operator
+                 "*": operator.mul}     # module. Saves us a few lines later on
     correct = 0
 ############# Choix de l'utilisateur #############
     choix = False
@@ -30,12 +33,17 @@ def jeuAM(c):
         else:
             print("Je n'ai pas bien compris votre choix. Veuillez choisir entre" 
             " l'option 0 ou 1. ")
+            # Returns a boolean value (here: False). This will be used later on.
             return choix
 
 ############# Jeu #############
-    typeOp = operators[op]
-
+    typeOp = operators[op]          # Takes the value of "op" as the key and 
+                                    # returns the assigned description (+ or *
+                                    # operators). The operator module then
+                                    # creates a function typeOp that returns
+                                    # the specified operation.
     for i in range(0, 10):
+        # Assign a random int value between 0 and 9 to a and b.
         a = randint(0, 9)
         b = randint(0, 9)
 
@@ -47,26 +55,34 @@ def jeuAM(c):
             correct = correct + 1
         else:
             print("Incorrect - la réponse est", real)
+
+    # If the program does not stop at the first else in the choosing section,
+    # returns an int.
     return correct
 
+##################### Main #########################
+
+# Initialize bonne as False and do not stop the loop until bonne is True
 bonne = False
 while not bonne:
-    try:
+# Run code and do something else if the program runs into a specified exception
+    try:  
         choice = int(input("0 = add, 1 = mult... "))
         bonne = jeuAM(choice)
 
-        if type(bonne) is bool:
-            continue
+        if type(bonne) is bool:     # If bonne is a boolean
+            continue                # try (loop) again
         elif bonne > 6:
             print(bonne, "réponses correctes. \nFélicitations!")
         else:
             print(bonne, "réponses correctes. \nDemandez à votre enseignant(e)" 
             " pour vous aider.")
-            break
+            break                   # Forces loop to stop. Otherwise when bonne
+                                    # = 0, the program keeps looping.
 
-    except ValueError:
+    except ValueError:              # If user inputs non numerical value
         print("Je n'ai pas bien compris votre choix...")
-    except KeyboardInterrupt:
+    except KeyboardInterrupt:       # If user exits with ^C
         print("\nAurevoir!")
         sys.exit()
 
