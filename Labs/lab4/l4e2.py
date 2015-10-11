c28b6ea10232e4e0af3caabc8f712613a45f7e1a
Félix Chiasson <github@felixchiasson.ca>
2015-10-11 10:48:32 -0400

    Fonction

--- Labs/lab4/l4e2.py
+++ Labs/lab4/l4e2.py
@@ -3,12 +3,13 @@
 #     File Name           :     l4e2.py
 #     Created By          :     Félix Chiasson (7138723)
 #     Creation Date       :     [2015-10-06 11:35]
-#     Last Modified       :     [2015-10-06 11:39]
+#     Last Modified       :     [2015-10-06 12:45]
 #     Description         :     Print numbers from 1 to N  
 ###############################################################################
-
+def bla(b):
+    i = 1
+    while i <= b:
+        print(i,end=" ")
+        i = i + 1
 n = int(input("Veuillez entrer un numéro: "))
-i = 1
-while i <= n:
-    print(i,end=" ")
-    i = i + 1
+bla(n)
