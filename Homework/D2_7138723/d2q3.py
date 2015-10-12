8fa8f540eaf21c04fd9a55a57f10286e6ae5e787
Félix Chiasson <github@felixchiasson.ca>
2015-10-12 16:53:19 -0400

    More error detection. Now counts letters and symbols as wrong answers. This fixes the issue where a user would input a letter and have to start from the beginning again. Quitting (^C) in the middle of the game will now give the user's score and a quit message.

--- Homework/D2_7138723/d2q3.py
+++ Homework/D2_7138723/d2q3.py
@@ -3,7 +3,7 @@
 #     File Name           :     d2q3.py
 #     Created By          :     Félix Chiasson (7138723)
 #     Creation Date       :     [2015-10-11 10:56]
-#     Last Modified       :     [2015-10-11 13:13]
+#     Last Modified       :     [2015-10-12 16:49]
 #     Description         :     Outil d'apprentissage de multiplications et
 #                               d'addition
 ###############################################################################
@@ -14,12 +14,12 @@ from random import randint
 
 def jeuAM(c):
 
-############# Initialisation des variables locales ###############
+    ############# Initialisation des variables locales ###############
     
     operators = {"+": operator.add,     # Dictionnary used with the operator
                  "*": operator.mul}     # module. Saves us a few lines later on
     correct = 0
-############# Choix de l'utilisateur #############
+    ############# Choix de l'utilisateur #############
     choix = False
     while not choix:
         if c == 0:
@@ -36,23 +36,30 @@ def jeuAM(c):
             # Returns a boolean value (here: False). This will be used later on.
             return choix
 
-############# Jeu #############
+    ############# Jeu #############
     typeOp = operators[op]          # Takes the value of "op" as the key and 
                                     # returns the assigned function (add() or
                                     # mul()) and assigns it to typeOp.
     for i in range(0, 10):
-        # Assign a random int value between 0 and 9 to a and b.
-        a = randint(0, 9)
-        b = randint(0, 9)
+        try:
+            # Assign a random int value between 0 and 9 to a and b.
+            a = randint(0, 9)
+            b = randint(0, 9)
 
-        print(a, op, b, '= ', end=' ')
-        reponse = int(input())
-        real = typeOp(a, b)
+            print(a, op, b, '= ', end=' ')
+            real = typeOp(a, b)
+            reponse = int(input())
 
-        if reponse == real:
-            correct = correct + 1
-        else:
+            if reponse == real:
+                correct = correct + 1
+            else:
+                print("Incorrect - la réponse est", real)
+        except ValueError:      # si la réponse n'est pas un chiffre
             print("Incorrect - la réponse est", real)
+        except KeyboardInterrupt:
+            print("\nDommage que vous partiez si tôt. Vous aviez", correct,
+                    "bonne(s) réponse(s).")
+            sys.exit()
 
     # If the program does not stop at the first else in the choosing section,
     # returns an int.
@@ -67,22 +74,24 @@ print("Ce logiciel va tester votre connaissance des opérations d'addition et "
         "1) Multiplication (0 ou 1)")
 while not bonne:
 # Run code and do something else if the program runs into a specified exception
-    try:  
+    
+    try:
         choice = int(input())
         bonne = jeuAM(choice)
 
         if type(bonne) is bool:     # If bonne is a boolean
-            continue                # try (loop) again
+            continue                # try (loop) again (see jeuAM())
         elif bonne > 6:
             print(bonne, "réponses correctes. \nFélicitations!")
         else:
-            print(bonne, "réponses correctes. \nDemandez à votre enseignant(e)" 
-            " pour vous aider.")
+            print(bonne, "réponse(s) correcte(s). \nDemandez à votre " 
+            "enseignant(e) pour vous aider.")
             break                   # Forces loop to stop. Otherwise when bonne
                                     # = 0 (False), the program keeps looping.
 
     except ValueError:              # If user inputs non numerical value
-        print("Je n'ai pas bien compris votre choix...")
+        print("Vous avez entré un valeur qui n'est pas un chiffre. \nVeuillez "
+                "faire votre choix (0 ou 1) à nouveau")
     except KeyboardInterrupt:       # If user exits with ^C
         print("\nAurevoir!")
         sys.exit()
